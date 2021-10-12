ac39543db0a22b8bfec3ef03fc40f056f3bd01c5
Mengxiao Qi <mengxiaoqi@MacBook-Air.local>
2021-10-11 21:59:12 -0500

    add app.post to get note input

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.port || 3001;
 const app = express();
 const notes = require('./db/db.json');
+const uuid = require('uuid');
+
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -21,6 +24,15 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
      
+     const {title, text} = req.body;
+     if (title && text) {
+          const newNote = {
+               title,
+               text,
+               id: uuid.v1()
+          }
+          console.log(newNote);
+     }
 });
 
 
